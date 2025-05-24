2c91562dd09e5937c7b731b287404785f48db529
Fabio Medeiros <feomedeirosdev@gmail.com>
2025-05-24 10:51:54 -0300

    feat: atualizações da query

--- ep2/query1.sql
+++ ep2/query1.sql
@@ -76,7 +76,66 @@ SELECT t2.seller_id AS "Seller ID",
 
   printf('%.4f', 
     CAST(count(t2.product_id) AS FLOAT) / count(DISTINCT t2.order_id)
-    ) AS "Avg Nun Products per Sale"
+    ) AS "Avg Nun Products per Sale",
+
+  sum(
+    CASE WHEN t4.product_category_name = 'automotivo' THEN 1 ELSE 0 END
+    ) AS "automotivo Category (qt)",
+
+  sum(
+    CASE WHEN t4.product_category_name = 'automotivo' THEN t2.price ELSE 0 END
+    ) AS '"automotivo" Category Partial Ravenue (R$)',
+
+  sum(CASE WHEN product_category_name = 'cama_mesa_banho' THEN 1 ELSE 0 END) AS "Cama Mesa Banho Category Sales (qt)",
+  sum(CASE WHEN product_category_name = 'beleza_saude' THEN 1 ELSE 0 END) AS "Beleza Saude Category Sales (qt)",
+  sum(CASE WHEN product_category_name = 'esporte_lazer' THEN 1 ELSE 0 END) AS "Esporte Lazer Category Sales (qt)",
+  sum(CASE WHEN product_category_name = 'moveis_decoracao' THEN 1 ELSE 0 END) AS "Moveis Decoracao Category Sales (qt)",
+  sum(CASE WHEN product_category_name = 'informatica_acessorios' THEN 1 ELSE 0 END) AS "Informatica Acessorios Category Sales (qt)",
+  sum(CASE WHEN product_category_name = 'utilidades_domesticas' THEN 1 ELSE 0 END) AS "Utilidades Domesticas Category Sales (qt)",
+  sum(CASE WHEN product_category_name = 'relogios_presentes' THEN 1 ELSE 0 END) AS "Relogios Presentes Category Sales (qt)",
+  sum(CASE WHEN product_category_name = 'telefonia' THEN 1 ELSE 0 END) AS "Telefonia Category Sales (qt)",
+  sum(CASE WHEN product_category_name = 'ferramentas_jardim' THEN 1 ELSE 0 END) AS "Ferramentas Jardim Category Sales (qt)",
+  sum(CASE WHEN product_category_name = 'automotivo' THEN 1 ELSE 0 END) AS "Automotivo Category Sales (qt)",
+  sum(CASE WHEN product_category_name = 'brinquedos' THEN 1 ELSE 0 END) AS "Brinquedos Category Sales (qt)",
+  sum(CASE WHEN product_category_name = 'cool_stuff' THEN 1 ELSE 0 END) AS "Cool Stuff Category Sales (qt)",
+  sum(CASE WHEN product_category_name = 'perfumaria' THEN 1 ELSE 0 END) AS "Perfumaria Category Sales (qt)",
+  sum(CASE WHEN product_category_name = 'bebes' THEN 1 ELSE 0 END) AS "Bebes Category Sales (qt)",
+  sum(CASE WHEN product_category_name = 'eletronicos' THEN 1 ELSE 0 END) AS "Eletronicos Category Sales (qt)",
+  sum(CASE WHEN product_category_name = 'papelaria' THEN 1 ELSE 0 END) AS "Papelaria Category Sales (qt)",
+  sum(CASE WHEN product_category_name = 'fashion_bolsas_e_acessorios' THEN 1 ELSE 0 END) AS "Fashion Bolsas e Acessorios Category Sales (qt)",
+  sum(CASE WHEN product_category_name = 'pet_shop' THEN 1 ELSE 0 END) AS "Pet Shop Category Sales (qt)",
+  sum(CASE WHEN product_category_name = 'moveis_escritorio' THEN 1 ELSE 0 END) AS "Moveis Escritorio Category Sales (qt)",
+  sum(CASE WHEN product_category_name = 'consoles_games' THEN 1 ELSE 0 END) AS "Consoles Games Category Sales (qt)",
+  sum(CASE WHEN product_category_name = 'malas_acessorios' THEN 1 ELSE 0 END) AS "Malas Acessorios Category Sales (qt)",
+  sum(CASE WHEN product_category_name = 'construcao_ferramentas_construcao' THEN 1 ELSE 0 END) AS "Construcao Ferramentas Construcao Category Sales (qt)",
+  sum(CASE WHEN product_category_name = 'eletrodomesticos' THEN 1 ELSE 0 END) AS "Eletrodomesticos Category Sales (qt)",
+  sum(CASE WHEN product_category_name = 'instrumentos_musicais' THEN 1 ELSE 0 END) AS "Instrumentos Musicais Category Sales (qt)",
+  sum(CASE WHEN product_category_name = 'eletroportateis' THEN 1 ELSE 0 END) AS "Eletroportateis Category Sales (qt)",
+  sum(CASE WHEN product_category_name = 'casa_construcao' THEN 1 ELSE 0 END) AS "Casa Construcao Category Sales (qt)",
+  sum(CASE WHEN product_category_name = 'livros_interesse_geral' THEN 1 ELSE 0 END) AS "Livros Interesse Geral Category Sales (qt)",
+  sum(CASE WHEN product_category_name = 'alimentos' THEN 1 ELSE 0 END) AS "Alimentos Category Sales (qt)",
+  sum(CASE WHEN product_category_name = 'moveis_sala' THEN 1 ELSE 0 END) AS "Moveis Sala Category Sales (qt)",
+  sum(CASE WHEN product_category_name = 'casa_conforto' THEN 1 ELSE 0 END) AS "Casa Conforto Category Sales (qt)",
+  sum(CASE WHEN product_category_name = 'bebidas' THEN 1 ELSE 0 END) AS "Bebidas Category Sales (qt)",
+  sum(CASE WHEN product_category_name = 'audio' THEN 1 ELSE 0 END) AS "Audio Category Sales (qt)",
+  sum(CASE WHEN product_category_name = 'market_place' THEN 1 ELSE 0 END) AS "Market Place Category Sales (qt)",
+  sum(CASE WHEN product_category_name = 'construcao_ferramentas_iluminacao' THEN 1 ELSE 0 END) AS "Construcao Ferramentas Iluminacao Category Sales (qt)",
+  sum(CASE WHEN product_category_name = 'climatizacao' THEN 1 ELSE 0 END) AS "Climatizacao Category Sales (qt)",
+  sum(CASE WHEN product_category_name = 'moveis_cozinha_area_de_servico_jantar_e_jardim' THEN 1 ELSE 0 END) AS "Moveis Cozinha Area de Servico Jantar e Jardim Category Sales (qt)",
+  sum(CASE WHEN product_category_name = 'alimentos_bebidas' THEN 1 ELSE 0 END) AS "Alimentos Bebidas Category Sales (qt)",
+  sum(CASE WHEN product_category_name = 'industria_comercio_e_negocios' THEN 1 ELSE 0 END) AS "Industria Comercio e Negocios Category Sales (qt)",
+  sum(CASE WHEN product_category_name = 'livros_tecnicos' THEN 1 ELSE 0 END) AS "Livros Tecnicos Category Sales (qt)",
+  sum(CASE WHEN product_category_name = 'telefonia_fixa' THEN 1 ELSE 0 END) AS "Telefonia Fixa Category Sales (qt)",
+  sum(CASE WHEN product_category_name = 'fashion_calcados' THEN 1 ELSE 0 END) AS "Fashion Calcados Category Sales (qt)",
+  sum(CASE WHEN product_category_name = 'eletrodomesticos_2' THEN 1 ELSE 0 END) AS "Eletrodomesticos 2 Category Sales (qt)",
+  sum(CASE WHEN product_category_name = 'construcao_ferramentas_jardim' THEN 1 ELSE 0 END) AS "Construcao Ferramentas Jardim Category Sales (qt)",
+  sum(CASE WHEN product_category_name = 'agro_industria_e_comercio' THEN 1 ELSE 0 END) AS "Agro Industria e Comercio Category Sales (qt)",
+  sum(CASE WHEN product_category_name = 'artes' THEN 1 ELSE 0 END) AS "Artes Category Sales (qt)",
+  sum(CASE WHEN product_category_name = 'pcs' THEN 1 ELSE 0 END) AS "PCs Category Sales (qt)",
+  sum(CASE WHEN product_category_name = 'sinalizacao_e_seguranca' THEN 1 ELSE 0 END) AS "Sinalizacao e Seguranca Category Sales (qt)",
+  sum(CASE WHEN product_category_name = 'construcao_ferramentas_seguranca' THEN 1 ELSE 0 END) AS "Construcao Ferramentas Seguranca Category Sales (qt)",
+  sum(CASE WHEN product_category_name = 'artigos_de_natal' THEN 1 ELSE 0 END) AS "Artigos de Natal Category Sales (qt)",
+  sum(CASE WHEN product_category_name = 'fashion_roupa_masculina' THEN 1 ELSE 0 END) AS "Fashion Roupa Masculina Category Sales (qt)"
 
 FROM tb_orders AS t1
 
@@ -90,6 +149,11 @@ LEFT JOIN
   ON
   t2.seller_id = t3.seller_id
 
+LEFT JOIN
+  tb_products AS t4
+  ON
+  t2.product_id = t4.product_id
+
 WHERE
   t1.order_approved_at >= '2016-10-01'
   AND
@@ -99,4 +163,5 @@ WHERE
 
 GROUP BY t2.seller_id
 
-LIMIT 10
+LIMIT 3
+
